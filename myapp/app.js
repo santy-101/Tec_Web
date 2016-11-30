e0d8934bd16e1d2f156e0151243a12415e9f82da
santy-101 <santiago.lema01@epn.edu.ec>
2016-11-30 13:36:10 -0500

    Con javascript

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/TecnologiasWeb', function (req, res) {
+  res.send('con javascript!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(5050, function () {
+  console.log('Example app listening on port 5050!')
+})
 })
