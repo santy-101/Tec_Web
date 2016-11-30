9b9a284092d8a99a88617a684d110276b755688e
santy-101 <santiago.lema01@epn.edu.ec>
2016-11-30 13:40:51 -0500

    Con put

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.get('/TecnologiasWeb', function (req, res) {
-  res.send('con javascript!')
+app.post('/TecnologiasWeb', function (req, res) {
+    res.send('con put!')
 })
 
 app.listen(5050, function () {
-  console.log('Example app listening on port 5050!')
+console.log('Example app listening on port 5050!')
 })
-})
+
