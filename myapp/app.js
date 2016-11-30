d153d7ce62cf11fa164c5bbc1d8398a9709d43cd
santy-101 <santiago.lema01@epn.edu.ec>
2016-11-30 14:44:53 -0500

    Usuarios

--- myapp/app.js
+++ myapp/app.js
@@ -1,47 +1,59 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+
+var app = express();
+
 var puerto = 6060;
 
 var usuarios = [
     {
         id:1,
-        nombre:'Pepe',
-        cedula:'123456782'
+        nombre:'Santiago',
+        cedula:'123409182'
     },
-     {
+    {
         id:2,
-        nombre:'Carlos',
-        cedula:'123454782'
+        nombre:'Fernando',
+        cedula:'981237918'
     },
-     {
+    {
         id:3,
-        nombre:'<Juan',
-        cedula:'120456782'
+        nombre:'Danny',
+        cedula:'011283934'
     }
-
 ]
 
 
-app.get('/Usuario/:idUsuario', function (req, res) {
-  
-    var idActual = req.params.idUsuario;
-    for(var i=0, i<usuarios.length;i++)
-        {
-            if(idActual === usuarios[i].id)
-                {
-                    res.json(usuarios[i]);
-                }
-        }
-    
-    res.send('No existe el usuario');
+
+app.get('/', function (req, res) {
+    res.send('Hola Mundo!')
 })
 
+app.get('/Usuario', function (req, res) {
+
+    res.json(usuarios);
+    
+})
+
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    
+    var idActual = req.params.idUsuario;
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario');
+    
+})
 
 app.get('/TecnologiasWeb', function (req, res) {
-    res.send('con javascript!')
+    res.send('con Javascript!')
 })
 
+
 app.post('/TecnologiasWeb', function (req, res) {
+
     //request => req
     //response => res
     
@@ -49,46 +61,58 @@ app.post('/TecnologiasWeb', function (req, res) {
     
     console.log(parametros);
     
-    
     var usuario = {
         nombre:'Santiago',
-        cedula:'000000'
-    
+        cedula:'000000000'
     }
-//    
+    
+    
+    res.append('token', '1234');
+    res.json(usuario);
+    
+    
 //    usuario = {
-//        
 //        nombre:usuario.nombre,
 //        cedula:usuario.cedula,
 //        apellido:''
-//        
 //    }
     
-//    usuario.apellido = 'Lema'
-//    usuario.mascotas = [];
-//    usuario.casado = false;
-//    
-//    console.log('Lo que tengo en el REQUEST es: ');  
+    
+    
+    
+    
+    
+    
+    
+//    console.log('Lo que tengo en el REQUEST es:')
 //    console.log(req);
-//    console.log('Lo que tengo en el RESPONSE es: ');  
+//    console.log('Lo que tengo en el RESPONSE es:')
 //    console.log(res);
 //    
-//    console.log('Cabeceras del REQUEST ');  
+//    console.log('Cabeceras Req:')
 //    console.log(req.headers);
-//   
-//    console.log('Cabeceras del RESPONSE ');  
-    
-    
-    res.append ('token','1234');
-    
-    console.log(res.headers);
-    
-    
+//    console.log('Cabeceras Res:')
+//    console.log(res.headers);
+//    
 
-    res.json(usuario);
+  
+    
+//    res.send('{nombre:Santiago,cedula:1716698079}');
+    
+//    if(si los minutos son pares){
+//        res.send('minutos pares')
+//    }else{
+//        res.send('minutos impares')
+//    }
+
+    
+    //res.send('Picachu');
+
+
 })
+
+
 
 app.listen(puerto, function () {
-console.log('Example app listening on port ' +puerto)
+    console.log('Example app listening on port ' + puerto + '!')
 })
-
