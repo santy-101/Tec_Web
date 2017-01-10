c69293e465352a16e4bbfa2d54c3fedd6b9db4d7
santy-101 <santiago.lema01@epn.edu.ec>
2017-01-10 14:18:13 -0500

    Simulación de forms

--- Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
+++ Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
@@ -10,22 +10,53 @@
 module.exports = {
 
     crearUsuario: function (req, res) {
-        Usuario.create({
-            nombres: 'Pepe José',
-            apellidos: 'Icaza Rey',
-            correo: 'pepe.icaza@gmail.com'
-        }).exec(function (err, usuarioCreado) {
-            
-            
-            
-            
-            if (err) {
-                return res.serverError(err);
-            }
 
-            sails.log('Finn\'s id is:', finn.id);
-            return res.ok();
-        });
+        //Guardando todos los parámetros en la variable parámetros
+
+        var parametros = req.allParams();
+
+        console.log(parametros);
+
+        if (parametros.nombres && parametros.apellidos)
+
+        {
+            //creo el usuario
+
+            Usuario.create({
+                nombres: parametros.nombres,
+                apellidos: parametros.apellidos,
+                correo: parametros.correo
+            }).exec(function (err, usuarioCreado) {
+
+                if (err) {
+                    return res.serverError(err);
+                }
+
+                sails.log.info(usuarioCreado);
+                return res.ok(usuarioCreado);
+            });
+
+        } else {
+            // bad request
+            return res.badRequest('Llene su nombre y apellido');
+
+        }
+//        Usuario.create({
+//            nombres: 'Pepe José',
+//            apellidos: 'Icaza Rey',
+//            correo: 'pepe.icaza@gmail.com'
+//        }).exec(function (err, usuarioCreado) {
+//
+//
+//
+//
+//            if (err) {
+//                return res.serverError(err);
+//            }
+//
+//            sails.log.info(usuarioCreado);
+//            return res.ok(usuarioCreado);
+//        });
     }
 
 };
