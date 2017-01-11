cc4c5ba5fd7948a36255670d48da34cbe3081e98
santy-101 <santiago.lema01@epn.edu.ec>
2017-01-11 13:42:57 -0500

    Combinar HTML y JS

---
+++ Servidor HTTP/Mascotas/api/controllers/RutasController.js
@@ -0,0 +1,26 @@
+/**
+ * RutasController
+ *
+ * @description :: Server-side logic for managing Rutas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    
+    home: function(req,res)
+    {
+        //res.view (String: Nombre vista, Datos JSON)
+        return res.view('vistas/home',{
+            titulo: 'Inicio',
+            numero:1,
+            mauricio:
+            {
+                nombre:'Mauricio',
+                apellido:'Larco'
+            }
+            
+        })
+    }
+	
+};
+
